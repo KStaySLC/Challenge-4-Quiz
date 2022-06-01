c085429d583b0291aaf9d8518a766ab0679c13b7
Kevin Stayner <kstayslc@icloud.com>
2022-06-01 14:42:27 -0600

    Bug fixes with timer and question display

--- script.js
+++ script.js
@@ -1,64 +1,97 @@
-var timer = document.getElementById("header1");
-var sec = 45;
-var timerID = setInterval(1000);
-
-function countDownTimer() {
-    if (sec == -1) {
-        clearTimeout(timerID);
-        // "DO Something this is where the question switches"
-    } else {
-        timer.innerHTML = sec + ' seconds remaining!';
-        sec--;
-    }
-}
-
-
-var startButton = document.getElementById('start');
-startButton.addEventListener('click', startTimer, countDownTimer);
-var questionsBox = document.getElementById('question-container');
-
-var questionRandom = 
-var currentQuestion = 
-
-
-
-function startTimer(quiz) {
-    startButton.classList.add('hide');
-    questionRandom = questions.sort(() => Math.random() - .5);
-    currentQuestion = 0
-    questionsBox.classList.remove('hide');
-    nextQuestion();
-    
-}
-
-var questions = [
-    {
+let timer = document.getElementById("header1");
+let sec = 30;
+let startButton = document.getElementById('start');
+startButton.addEventListener('click', startTimer);
+let questionsBox = document.getElementById('question-container');
+let questions = [
+    question1={
         question: "What does HTML stand for?",
         answers: [
-            { text: 'HyperText Markup Language', correct: true },
-            { text: 'HydroThermal Medeteranian Logistics', correct: false},
-            { text: 'Herculean Tasks Made Lazy', correct: false},
-            { text: 'Hateful Thoughtless Manditory Lists', correct: false},
-        ]
+            'HyperText Markup Language', 
+            'HydroThermal Medeteranian Logistics', 
+            'Herculean Tasks Made Lazy',
+            'Hateful Thoughtless Manditory Lists',
+        ],
+        correctAnsw:'HyperText Markup Language'
+    },
+    question2={
         question: "If text is wrapped in quotation marks it is considered what?",
         answers: [
-            {text: 'Number', correct: false},
-            {text: 'String', correct: true},
-            {text: 'Boolean', correct: false},
-            {text: 'Undefined', correct: false},
-        ]
-        question: "My dog's name is...?"
+           'Number',
+           'String',
+           'Boolean',
+           'Undefined',
+        ],
+        correctAnsw:'String'
+    }
+    question3= {
+        question: "My dog is named what?",
         answers: [
-            {text: 'Canon', correct: true},
-            {text: 'Canon', correct: true},
-            {text: 'Canon', correct: true},
-            {text: 'Canon', correct: true},
-        ]
-
+            'Canon',
+            'Canon',
+            'Canon',
+            'Canon',
+        ],
+        correctAnsw: 'Canon'
     }
 ]
-function nextQuestion() {
 
+let numberCorrect;
+let numberIncorrect;
+let questionArrayOrder;
+let questionIndex;
+
+let pageArray = [startButton, questionsBox]
+
+function countDownTimer() {
+    
+    let timeLeft = setInterval(() => {
+        timer.innerHTML = sec + ' seconds remaining!';
+        sec--;
+        if (sec < 0) {
+            clearTimeout(timeLeft);
+            timer.innerHTML = 'Time is up!'
+        } 
+    }, 1000);
+}
+
+function hidePage(){
+    for (let i = 0; i < pageArray.length; i++) {
+        if(!pageArray[i].classList.contains('hide')){
+            pageArray[i].classList.add('hide');
+        }
+    }
+}
+
+function shuffle(array){
+    return array.sort(() => Math.random() - .5);
+}
+
+function questionsOrder(array){
+    let arrayIndex = [];
+
+    for (let i = 0; i < array.length; i++) {
+        arrayIndex.push(i);
+    }
+    return shuffle(arrayIndex)
+}
+
+
+function startTimer() {
+    numberCorrect = 0;
+    numberIncorrect = 0
+    countDownTimer()
+    hidePage()
+    currentQuestion = 0
+    questionsBox.classList.remove('hide')
+    questionIndex = 0;
+    questionArrayOrder = questionsOrder(questions)
+    nextQuestion(questions, questionArrayOrder[questionIndex]);
+    
+}
+function nextQuestion(array, index) {
+    console.log('array ', array , 'index ', index)
+    // showQuestion(question)
 }
 
 function correctAnswer() {
