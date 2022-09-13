8f6888e9a6ed4524c8c17bd0bc6d8f88818c6110
Kevin Stayner <kstayslc@icloud.com>
2022-09-13 15:37:03 -0600

    Debugging

--- script.js
+++ script.js
@@ -1,50 +1,70 @@
 let timer = document.getElementById("header1");
-let sec = 30;
-let startButton = document.getElementById('start');
-startButton.addEventListener('click', startTimer);
+let sec = 45;
+
+const startButton = document.getElementById('start');
+startButton.addEventListener('click', startTimer, nextQuestion);
+
 let questionsBox = document.getElementById('question-container');
-let questions = [
-    question1={
+
+const answers = document.getElementById('answers');
+
+let randomQuestion, questionIndex;
+let score = 0;
+
+
+const questions = [
+    {
         question: "What does HTML stand for?",
         answers: [
-            'HyperText Markup Language', 
-            'HydroThermal Medeteranian Logistics', 
-            'Herculean Tasks Made Lazy',
-            'Hateful Thoughtless Manditory Lists',
-        ],
-        correctAnsw:'HyperText Markup Language'
+            'HyperText Markup Language',
+            'HydroThermal Mediterranean Logistics', 
+            'Herculean Tasks Made Lazy', 
+            'Hateful Thoughtless Mandatory Lists', 
+        ], 
+        correct: 'HyperText Markup Language'
     },
-    question2={
+    {
         question: "If text is wrapped in quotation marks it is considered what?",
         answers: [
-           'Number',
-           'String',
-           'Boolean',
-           'Undefined',
-        ],
-        correctAnsw:'String'
-    }
-    question3= {
+           'Number',  
+           'String', 
+           'Boolean',  
+           'Undefined',  
+        ], 
+        correct: 'String'
+    },
+    {
         question: "My dog is named what?",
         answers: [
-            'Canon',
-            'Canon',
-            'Canon',
-            'Canon',
-        ],
-        correctAnsw: 'Canon'
-    }
-]
+           'Canon',  
+           'Canon',  
+           'Canon',  
+           'Canon',  
+        ], 
+        correct: 'Canon'
+    },
+    {
+        question: "What does a cascading style sheet do?",
+        answers: [
+           'Make developers loose their minds', 
+           'Style JavaScript applications for an evening out', 
+           'Create a more appealing asthenic for Node.Js', 
+           'They are demons escaped from hell and should be avoided at all costs',
+
+        ], 
+        correct: 'Make developers loose their minds' || 'They are demons escaped from hell and should be avoided at all costs'
+    },
+];
 
 let numberCorrect;
 let numberIncorrect;
 let questionArrayOrder;
-let questionIndex;
+
 
 let pageArray = [startButton, questionsBox]
 
 function countDownTimer() {
-    
+   
     let timeLeft = setInterval(() => {
         timer.innerHTML = sec + ' seconds remaining!';
         sec--;
@@ -70,7 +90,7 @@ function shuffle(array){
 function questionsOrder(array){
     let arrayIndex = [];
 
-    for (let i = 0; i < array.length; i++) {
+    for (let i = 0; i < questions.length; i++) {
         arrayIndex.push(i);
     }
     return shuffle(arrayIndex)
@@ -90,10 +110,18 @@ function startTimer() {
     
 }
 function nextQuestion(array, index) {
-    console.log('array ', array , 'index ', index)
-    // showQuestion(question)
-}
+    let displayQuestion = 
+    document.getElementById('question');
+    displayQuestion.innerHTML = questions[0].question;
 
-function correctAnswer() {
+    let displayAnswer = 
+    document.getElementsByClassName('.button');
+    console.log('got this far...');
 
-}
+    for (let i = 0; i< answers.length; i++) {
+      displayAnswer.innerHTML = questions.answers[i];
+        
+    }  
+    console.log('but not this far...')
+};
+
